fb1c2952d8860bff5944266ff75f3fd59be6514e
gbayarri <genis.bayarri@irbbarcelona.org>
2023-11-02 13:06:43 +0100

    Adjusting styles

--- docs/source/_static/theme_overrides.js
+++ docs/source/_static/theme_overrides.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
 	if($('.wy-side-nav-search .icon.icon-home').attr('href') == '../index.html') path = '../';
 	else if($('.wy-side-nav-search .icon.icon-home').attr('href') == '../../index.html') path = '../../';
 		
-	$('.wy-side-nav-search .icon.icon-home').html('<img src="' + path + '_static/logo.png" class="logo" alt="Logo" style="width:110px;">');
+	$('.wy-side-nav-search .icon.icon-home').html('<img src="' + path + '_static/logo_white.png" class="logo" alt="Logo" style="width:110px;">');
 	$('.wy-breadcrumbs .icon.icon-home').html('biomovies');
 });
