8733acd7a81643e9a4fbf7417257fd4db527b142
gbayarri <genis.bayarri@irbbarcelona.org>
2023-11-03 09:49:21 +0100

    Test font awesome

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,7 +128,7 @@ todo_include_todos = False
 # -- Options for HTML output ----------------------------------------------
 def setup(app):
     app.add_css_file('theme_overrides.css')
-    app.add_css_file('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css')
+    # app.add_css_file('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css')
     app.add_js_file('theme_overrides.js')
 
 
