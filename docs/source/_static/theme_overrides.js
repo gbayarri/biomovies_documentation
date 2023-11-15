1b7486e4930c7484a0318c957682b6b543da40b8
gbayarri <genis.bayarri@irbbarcelona.org>
2023-11-15 16:58:26 +0100

    New Releases

--- docs/source/_static/theme_overrides.js
+++ docs/source/_static/theme_overrides.js
@@ -10,24 +10,31 @@ $(document).ready(function() {
 	// releases
 	if($( "h1" ).text().indexOf('Releases') != -1) {
 		var count = 1;
+		// get data from github releases
 		fetch('https://api.github.com/repos/gbayarri/biomovies-src/releases')
 			.then(res => res.json())
 			.then(json => {
 
 				json = json.reverse();
 
+				$( ".toctree-l1.current" ).append(`<ul></ul>`);
+
 				json.forEach(item => {
 
+					// format date
 					const d = new Date(item.published_at)
 					const publish_date = `${(d.getDate() < 10 ? '0' : '') + d.getDate()}/${(d.getMonth() < 9 ? '0' : '') + (parseInt(d.getMonth()) + 1)}/${d.getFullYear()}`
 
+					// format body
 					const body = item.body.split('\r\n')
 
 					let type = ''
 					let list_body = []
 
+					// get full changelog url
 					const full_changelog = body.filter(item => item.indexOf('Full Changelog') !== -1)[0].split(': ')
 
+					// get type and list body
 					body.forEach((item, index) => {
 						if(index == 0) {
 							if(item.indexOf('Alpha') !== -1) type = 'Alpha'
@@ -39,6 +46,7 @@ $(document).ready(function() {
 						}
 					})
 
+					// add release section
 					$( "h1" ).after(`
 						<section id="id${count}">
 						<h2>${item.tag_name}<a class="headerlink" href="#id${count}" title="Link to this heading"></a></h2>
@@ -50,7 +58,7 @@ $(document).ready(function() {
 						</section>
 					`);
 
-					$( ".toctree-l1.current" ).append(`ul`);
+					// add link to sidebar
 					$( ".toctree-l1.current ul" ).prepend(`
 						<li class="toctree-l2"><a class="reference internal" href="#id${count}">${item.tag_name}</a></li>
 					`);
@@ -59,6 +67,7 @@ $(document).ready(function() {
 				})
 
 			}).catch(function() {
+				// if no data available from github releases show error message
 				$( "h1" ).after(`
 					<p>Unable to connect to GitHub Source code</p>
 				`);
